4a6db2d991585956343f1c0f985154e435fa2a94
GhulamFatima505 <107924461+GhulamFatim@users.noreply.github.com>
2022-09-24 18:34:14 -0700

    Update queue.cpp

--- queue.cpp
+++ queue.cpp
@@ -6,32 +6,34 @@ using namespace std;
 		
    	public:
 	   		
-	    Queue():front(-1),back(-1),size(100){}           //size(0)
+	   Queue():front(-1),back(-1),size(100){}           //size(0)
 	    
-        bool isEmpty(){return(front==-1||front>back);}  //(size==0 || front > back)
+           bool isEmpty(){return(front==-1||front>back);}  //(size==0 || front > back)
     
-		void Display(){
+	   void Display(){
 		    if(isEmpty())cout<<"Queue is empty"<<endl;
-            else {cout<<"Queue elements are : ";
-            for (int i = front; i <= back; i++)cout<<arr[i]<<" ";
-            cout<<endl;}
+                    else {cout<<"Queue elements are : ";
+                    for (int i = front; i <= back; i++)cout<<arr[i]<<" ";
+                    cout<<endl;}
 	    }
 	   
 	    bool isFull(){return (back>=size-1);}  //(size==99)
 	    
 	    bool enqueue(int val){
 		    if (back==size-1)return false;  //(size==99)
-            if (front==-1)front++;
-            back++;               //	back++;  size++;
-            arr[back]=val;
-            return true;
-		}
-        bool dequeue(){
-	    if(isEmpty())return false;
-	    front++;
-        return true;} 
-        int atFront(){if (isEmpty())return -1; 
-	    return arr[front];
+                    if (front==-1)front++;
+                    back++;               //	back++;  size++;
+                    arr[back]=val;
+                     return true;
+	    }
+            bool dequeue(){
+	           if(isEmpty())return false;
+	           front++;
+                   return true;} 
+	    
+           int atFront(){
+		   if (isEmpty())return -1; 
+	           return arr[front];
 		}
     };
 int main(){
