9b37ec4ac4688f3e8da6786f5f599270ebee3182
GhulamFatima505 <107924461+GhulamFatim@users.noreply.github.com>
2022-09-24 18:53:46 -0700

    Update Stack using array.cpp

--- Stack using array.cpp
+++ Stack using array.cpp
@@ -3,21 +3,28 @@ using namespace std;
 class Stack{
 int arr[100],size,top;
 public:
-	Stack():size(100),top(-1){}     //size(0)
+	Stack():size(100),top(-1){}//size(0)
+	
 	bool isEmpty(){return (top<0);}                    //(size==0);
-	void display() {if (isEmpty()) cout<<"Stack is empty"<<endl;
-       else{cout<<"Stack elements are : ";
-	   for(int i=top; i>=0; i--) cout<<arr[i]<<" ";
-       cout<<endl;}}         
+	
+	void display() {
+		if (isEmpty()) cout<<"Stack is empty"<<endl;
+                else{cout<<"Stack elements are : ";
+	        for(int i=top; i>=0; i--) cout<<arr[i]<<" ";
+                cout<<endl;}}         
+	
     bool isFull(){return (top>=size-1);}                       //return (size==99);
+	
     bool push(int val){                                       
       if(isFull()) return false;
 	  top++;                                              //         top++; size++;
       arr[top]=val;                                      //          arr[size]=val;
-      return true; }                                               
+      return true; }        
+	
     bool pop() {if(isEmpty()) return false;
 	  top--;                                       //         top--;  size--;	
       return true;  }  
+	
     int atTop(){if(isEmpty())return -1;                     
        return arr[top];	}	
 };
